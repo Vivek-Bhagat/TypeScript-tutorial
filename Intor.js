ae5cc46def93bbfc33c00128e614f027df3793ca
“Vivek <vivekbhagat1890@hotmail.com>
2023-08-28 00:15:00 +0530

    typescript started day -1

---
+++ Intor.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Greetings = "Hello vivek";
+var num = 7;
+// acccidental change by the user
+//num.toUppercase()
+Greetings.toUpperCase();
+console.log(Greetings);
---
+++ Intor.ts
@@ -0,0 +1,9 @@
+let Greetings: string ="Hello vivek"
+
+let num = 7;
+
+// acccidental change by the user
+//num.toUppercase()
+Greetings.toUpperCase();
+console.log(Greetings)
+export {}
