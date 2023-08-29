a7d9ee324013bec9d15e2ee623e31f962e973197
“Vivek <vivekbhagat1890@hotmail.com>
2023-08-29 18:55:01 +0530

    type Allias

---
+++ typeAlias.ts
@@ -0,0 +1,12 @@
+type User = {
+    name: string
+    email: string
+    isActive: boolean
+}
+
+function createUser(user: User): User {
+    return { name: "" , email: "" , isActive: true}   
+}
+
+createUser({name:" ", email:" " , isActive: true})
+export{}
